ccb8917f0f09ae708f3195e75846ebfaabcbea61
feng <1591293009@qq.com>
2015-09-21 10:30:34 +0800

    OneDay新建完毕

---
+++ OneDay/OneDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneDay
+//
+//  Created by feng on 15/9/21.
+//  Copyright (c) 2015年 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneDay/OneDay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneDay
+//
+//  Created by feng on 15/9/21.
+//  Copyright (c) 2015年 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
