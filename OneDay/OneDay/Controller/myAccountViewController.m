d7cd4dbc3a00bb69c56cb2d31c6d04a46cb7ae71
feng <1591293009@qq.com>
2015-10-10 10:49:17 +0800

    修复我的页面更换手机号的bug

--- OneDay/OneDay/Controller/myAccountViewController.m
+++ OneDay/OneDay/Controller/myAccountViewController.m
@@ -10,6 +10,7 @@
 #import "dayListViewController.h"
 
 @interface myAccountViewController ()
+- (IBAction)changphonenum:(UIButton *)sender forEvent:(UIEvent *)event;
 
 - (IBAction)logoutAction:(UIButton *)sender forEvent:(UIEvent *)event;
 
@@ -51,6 +52,33 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)changphonenum:(UIButton *)sender forEvent:(UIEvent *)event {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:[NSString stringWithFormat:@"请输入你的新的手机号"] delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定",nil];
+    alert.alertViewStyle= UIAlertViewStylePlainTextInput;
+    [alert show];
+}
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        UITextField *textField = [alertView textFieldAtIndex:0];
+        if ([textField.text isEqualToString:@""]) {
+            [Utilities popUpAlertViewWithMsg:@"请输入你的新的手机号" andTitle:nil];
+            return;//终止该方法操作
+        }
+        
+        PFUser *user=[PFUser currentUser];
+        user[@"PhoneNum"] = textField.text;
+        
+        [user saveInBackgroundWithBlock:^(BOOL succeeded,NSError *error) {
+            if (succeeded) {
+                [Utilities popUpAlertViewWithMsg:@"你已成功修改!" andTitle:nil];
+            }else
+            {
+                [Utilities popUpAlertViewWithMsg:nil andTitle:nil];
+            }
+        }];
+    }
+}
 
 - (IBAction)logoutAction:(UIButton *)sender forEvent:(UIEvent *)event {
     [PFUser logOut];//退出parse
